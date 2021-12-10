dc50ba68f8a30d98dcb6447626f45d83f860872d
dhirlea <63645495+dhirlea@users.noreply.github.com>
2021-12-10 10:59:13 +0000

    added image pre-processing functionality to improve quality of text extraction

---
+++ image_pre_processing.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+from image_to_text import convert_img_to_txt
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+ 
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+    
+#erosion
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+#canny edge detection
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+#skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+#template matching
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
+
+
+def main():
+    img = cv2.imread('nanonet.jpg')
+    gray = get_grayscale(img)
+    gray_3_channel = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR) # need to convert to 3 channels to concatenate with original image
+    image_gray_horizontal = np.hstack((img, gray_3_channel))
+
+    # Window name in which image is displayed
+    window_name = 'Transformed Nanonet Example Image'
+    
+    # Using cv2.imshow() method 
+    # Displaying the image 
+    cv2.imshow(window_name, image_gray_horizontal)
+    
+    #waits for user to press any key 
+    #(this is necessary to avoid Python kernel form crashing)
+    cv2.waitKey(0) 
+    
+    #closing all open windows 
+    cv2.destroyAllWindows() 
+
+    with open('nanonet.txt', 'w', encoding='utf-8') as f:
+        f.write(convert_img_to_txt(gray))
+
+if __name__ == '__main__':
+    main()
