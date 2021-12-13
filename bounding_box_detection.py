d68d8c86e6eef10caf87c2031f7cf5fd5f99ca14
dhirlea <63645495+dhirlea@users.noreply.github.com>
2021-12-13 16:48:33 +0000

    refactored script

--- bounding_box_detection.py
+++ bounding_box_detection.py
@@ -3,22 +3,36 @@ import re
 import pytesseract
 from pytesseract import Output
 
-def main():
+def load_image(img_path, config):
+    image = cv2.imread(img_path)
+    image_data = pytesseract.image_to_data(image, output_type=Output.DICT, config=config)
+    return image, image_data
 
-    img = cv2.imread('images/invoice.jpg')
-    d = pytesseract.image_to_data(img, output_type=Output.DICT)
-    n_boxes = len(d['text'])
+def plot_image_with_boxes(image, image_data):
+    n_boxes = len(image_data['text'])
     for i in range(n_boxes):
-        if int(float(d['conf'][i])) > 60:
-            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if int(float(image_data['conf'][i])) > 60:
+            (x, y, w, h) = (image_data['left'][i], image_data['top'][i], image_data['width'][i], image_data['height'][i])
+            img = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+
+def image_to_txt(image_data, file_path):
     alphanumeric = u'[a-zA-Z0-9]+'
-    document_text = [sequence for sequence in d['text'] if re.search(alphanumeric, sequence)]
-    with open('output_text_files/receipt.txt','w', encoding='utf-8') as f:
+    document_text = [sequence for sequence in image_data['text'] if re.search(alphanumeric, sequence)]
+    with open(file_path,'w', encoding='utf-8') as f:
         for sequence in document_text:
             f.write(sequence + '\n')
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
+
+def main():
+
+    img_path = 'images/invoice.jpg'
+    file_path = 'output_text_files/invoice.txt'
+    custom_config = r'--oem 3 --psm 3'
+    
+    img, image_data = load_image(img_path, custom_config)
+    image_to_txt(image_data, file_path)
+    plot_image_with_boxes(img, image_data)
 
 if __name__ =='__main__':
     main()
